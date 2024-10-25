c0d43fce0b8bf59642e2af53e33f101deb5ce7ce
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-10-25 06:49:36 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-white bg-purple-500 text-center rounded'>Vite + React</h1>
+      <button className='btn btn-primary'>Click me</button>
+    </>
+  )
+}
+
+export default App
